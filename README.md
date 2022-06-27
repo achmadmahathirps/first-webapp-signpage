9eeaf32717b25f4536abfc89848886ac5f32f1fc
Achmad Mahathir Prasanjaya <64342664+achmadmahathirps@users.noreply.github.com>
2022-06-27 12:03:08 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# first_webapp_signpage
+ My first try on implementing Flutter framework into web-based app.
---
+++ android/app/src/main/kotlin/com/example/first_webapp_signpage/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.first_webapp_signpage
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
